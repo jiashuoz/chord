5404244c9b72657ff59c2ab4907c6c2fefe61da1
Josh Zhang <jiashuoz@outlook.com>
2019-05-09 14:39:08 -0400

    updated chordrpc; merged kv rpc into chord rpc

--- chordrpc/chord.pb.go
+++ chordrpc/chord.pb.go
@@ -143,29 +143,278 @@ func (m *ID) GetId() []byte {
 	return nil
 }
 
+// For KV store
+type GetRequest struct {
+	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetRequest) Reset()         { *m = GetRequest{} }
+func (m *GetRequest) String() string { return proto.CompactTextString(m) }
+func (*GetRequest) ProtoMessage()    {}
+func (*GetRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{3}
+}
+
+func (m *GetRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetRequest.Unmarshal(m, b)
+}
+func (m *GetRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetRequest.Marshal(b, m, deterministic)
+}
+func (m *GetRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetRequest.Merge(m, src)
+}
+func (m *GetRequest) XXX_Size() int {
+	return xxx_messageInfo_GetRequest.Size(m)
+}
+func (m *GetRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetRequest proto.InternalMessageInfo
+
+func (m *GetRequest) GetKey() string {
+	if m != nil {
+		return m.Key
+	}
+	return ""
+}
+
+type GetReply struct {
+	Val                  string   `protobuf:"bytes,1,opt,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetReply) Reset()         { *m = GetReply{} }
+func (m *GetReply) String() string { return proto.CompactTextString(m) }
+func (*GetReply) ProtoMessage()    {}
+func (*GetReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{4}
+}
+
+func (m *GetReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetReply.Unmarshal(m, b)
+}
+func (m *GetReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetReply.Marshal(b, m, deterministic)
+}
+func (m *GetReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetReply.Merge(m, src)
+}
+func (m *GetReply) XXX_Size() int {
+	return xxx_messageInfo_GetReply.Size(m)
+}
+func (m *GetReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetReply proto.InternalMessageInfo
+
+func (m *GetReply) GetVal() string {
+	if m != nil {
+		return m.Val
+	}
+	return ""
+}
+
+type PutRequest struct {
+	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	Val                  string   `protobuf:"bytes,2,opt,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PutRequest) Reset()         { *m = PutRequest{} }
+func (m *PutRequest) String() string { return proto.CompactTextString(m) }
+func (*PutRequest) ProtoMessage()    {}
+func (*PutRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{5}
+}
+
+func (m *PutRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PutRequest.Unmarshal(m, b)
+}
+func (m *PutRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PutRequest.Marshal(b, m, deterministic)
+}
+func (m *PutRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PutRequest.Merge(m, src)
+}
+func (m *PutRequest) XXX_Size() int {
+	return xxx_messageInfo_PutRequest.Size(m)
+}
+func (m *PutRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_PutRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PutRequest proto.InternalMessageInfo
+
+func (m *PutRequest) GetKey() string {
+	if m != nil {
+		return m.Key
+	}
+	return ""
+}
+
+func (m *PutRequest) GetVal() string {
+	if m != nil {
+		return m.Val
+	}
+	return ""
+}
+
+type PutReply struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PutReply) Reset()         { *m = PutReply{} }
+func (m *PutReply) String() string { return proto.CompactTextString(m) }
+func (*PutReply) ProtoMessage()    {}
+func (*PutReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{6}
+}
+
+func (m *PutReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PutReply.Unmarshal(m, b)
+}
+func (m *PutReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PutReply.Marshal(b, m, deterministic)
+}
+func (m *PutReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PutReply.Merge(m, src)
+}
+func (m *PutReply) XXX_Size() int {
+	return xxx_messageInfo_PutReply.Size(m)
+}
+func (m *PutReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_PutReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PutReply proto.InternalMessageInfo
+
+type DeleteRequest struct {
+	Key                  string   `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *DeleteRequest) Reset()         { *m = DeleteRequest{} }
+func (m *DeleteRequest) String() string { return proto.CompactTextString(m) }
+func (*DeleteRequest) ProtoMessage()    {}
+func (*DeleteRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{7}
+}
+
+func (m *DeleteRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_DeleteRequest.Unmarshal(m, b)
+}
+func (m *DeleteRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_DeleteRequest.Marshal(b, m, deterministic)
+}
+func (m *DeleteRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DeleteRequest.Merge(m, src)
+}
+func (m *DeleteRequest) XXX_Size() int {
+	return xxx_messageInfo_DeleteRequest.Size(m)
+}
+func (m *DeleteRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_DeleteRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_DeleteRequest proto.InternalMessageInfo
+
+func (m *DeleteRequest) GetKey() string {
+	if m != nil {
+		return m.Key
+	}
+	return ""
+}
+
+type DeleteReply struct {
+	Val                  string   `protobuf:"bytes,2,opt,name=val,proto3" json:"val,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *DeleteReply) Reset()         { *m = DeleteReply{} }
+func (m *DeleteReply) String() string { return proto.CompactTextString(m) }
+func (*DeleteReply) ProtoMessage()    {}
+func (*DeleteReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_541dae51990542ec, []int{8}
+}
+
+func (m *DeleteReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_DeleteReply.Unmarshal(m, b)
+}
+func (m *DeleteReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_DeleteReply.Marshal(b, m, deterministic)
+}
+func (m *DeleteReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DeleteReply.Merge(m, src)
+}
+func (m *DeleteReply) XXX_Size() int {
+	return xxx_messageInfo_DeleteReply.Size(m)
+}
+func (m *DeleteReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_DeleteReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_DeleteReply proto.InternalMessageInfo
+
+func (m *DeleteReply) GetVal() string {
+	if m != nil {
+		return m.Val
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*Node)(nil), "chordrpc.Node")
 	proto.RegisterType((*NN)(nil), "chordrpc.NN")
 	proto.RegisterType((*ID)(nil), "chordrpc.ID")
+	proto.RegisterType((*GetRequest)(nil), "chordrpc.GetRequest")
+	proto.RegisterType((*GetReply)(nil), "chordrpc.GetReply")
+	proto.RegisterType((*PutRequest)(nil), "chordrpc.PutRequest")
+	proto.RegisterType((*PutReply)(nil), "chordrpc.PutReply")
+	proto.RegisterType((*DeleteRequest)(nil), "chordrpc.DeleteRequest")
+	proto.RegisterType((*DeleteReply)(nil), "chordrpc.DeleteReply")
 }
 
 func init() { proto.RegisterFile("chord.proto", fileDescriptor_541dae51990542ec) }
 
 var fileDescriptor_541dae51990542ec = []byte{
-	// 206 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4e, 0xce, 0xc8, 0x2f,
-	0x4a, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x00, 0x73, 0x8a, 0x0a, 0x92, 0x95, 0xd4,
-	0xb8, 0x58, 0xfc, 0xf2, 0x53, 0x52, 0x85, 0xf8, 0xb8, 0x98, 0x32, 0x53, 0x24, 0x18, 0x15, 0x18,
-	0x35, 0x78, 0x82, 0x98, 0x32, 0x53, 0xc0, 0xfc, 0x02, 0x09, 0x26, 0x05, 0x46, 0x0d, 0xce, 0x20,
-	0xa6, 0xcc, 0x02, 0x25, 0x16, 0x2e, 0x26, 0x3f, 0x3f, 0x25, 0x11, 0x2e, 0x26, 0x4f, 0x17, 0x74,
-	0xb5, 0x46, 0xdf, 0x18, 0xb9, 0x58, 0x9d, 0x41, 0x06, 0x0a, 0xe9, 0x72, 0xf1, 0xba, 0x65, 0xe6,
-	0xa5, 0x04, 0x97, 0x26, 0x27, 0xa7, 0x16, 0x17, 0xe7, 0x17, 0x09, 0xf1, 0xe8, 0xc1, 0x6c, 0xd2,
-	0xf3, 0x74, 0x91, 0xe2, 0x43, 0xf0, 0xc0, 0x96, 0x5a, 0x70, 0x49, 0x80, 0x94, 0x3b, 0xe7, 0xe4,
-	0x17, 0xa7, 0x16, 0x97, 0x04, 0x14, 0xa5, 0x26, 0xa7, 0xa6, 0x64, 0xe6, 0xa5, 0x83, 0xe5, 0xf0,
-	0xeb, 0xd4, 0xe3, 0xe2, 0x73, 0x4f, 0x05, 0xe9, 0x48, 0x49, 0xc5, 0xb4, 0xc9, 0xcf, 0x0f, 0x43,
-	0xbd, 0x0e, 0x17, 0x8f, 0x7b, 0x6a, 0x09, 0x56, 0x77, 0x61, 0x51, 0xad, 0xc6, 0xc5, 0xe6, 0x97,
-	0x5f, 0x92, 0x99, 0x56, 0x29, 0x84, 0x26, 0x23, 0x85, 0xa2, 0x2f, 0x89, 0x0d, 0x1c, 0x9a, 0xc6,
-	0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x83, 0x8a, 0x08, 0x54, 0x5c, 0x01, 0x00, 0x00,
+	// 329 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0x5f, 0x4f, 0xf2, 0x30,
+	0x14, 0xc6, 0x43, 0xc7, 0x4b, 0xe0, 0xf0, 0x27, 0x6f, 0x1a, 0x8c, 0x64, 0x31, 0x8a, 0xbd, 0x20,
+	0x5c, 0xe8, 0x30, 0x7a, 0xc3, 0x3d, 0xc4, 0x85, 0x9b, 0x85, 0xcc, 0x4f, 0x80, 0xeb, 0x51, 0x17,
+	0x1b, 0x5a, 0xb7, 0x8e, 0x64, 0x5f, 0xc3, 0x4f, 0x6c, 0x5a, 0xb3, 0x14, 0x18, 0x78, 0xb7, 0xd3,
+	0xe7, 0xf9, 0xad, 0x27, 0xcf, 0x53, 0xe8, 0x26, 0x1f, 0x32, 0xe3, 0x81, 0xca, 0xa4, 0x96, 0xb4,
+	0x6d, 0x87, 0x4c, 0x25, 0x6c, 0x02, 0xcd, 0x48, 0x72, 0xa4, 0x03, 0x20, 0x29, 0x1f, 0x35, 0xc6,
+	0x8d, 0x69, 0x2f, 0x26, 0x29, 0xb7, 0xb3, 0x1a, 0x91, 0x71, 0x63, 0xda, 0x89, 0x49, 0xaa, 0x58,
+	0x13, 0x48, 0x14, 0xb1, 0x21, 0x90, 0xd5, 0xf2, 0xd8, 0xcb, 0xae, 0x01, 0x42, 0xd4, 0x31, 0x7e,
+	0x15, 0x98, 0x6b, 0xfa, 0x1f, 0xbc, 0x4f, 0x2c, 0xad, 0xdc, 0x89, 0xcd, 0x27, 0xbb, 0x82, 0xb6,
+	0xd5, 0x95, 0x28, 0x8d, 0xba, 0xdb, 0x88, 0x4a, 0xdd, 0x6d, 0x04, 0x7b, 0x00, 0x58, 0x17, 0xe7,
+	0xe9, 0x8a, 0x20, 0x8e, 0x00, 0x68, 0x5b, 0x42, 0x89, 0x92, 0xdd, 0x42, 0x7f, 0x89, 0x02, 0x35,
+	0x9e, 0xbf, 0xfe, 0x06, 0xba, 0x95, 0x65, 0x6f, 0x03, 0xf7, 0xbf, 0xc7, 0x6f, 0x0f, 0xfe, 0x2d,
+	0x4c, 0x20, 0xf4, 0x1e, 0xfa, 0xcf, 0xe9, 0x96, 0xbf, 0x14, 0x49, 0x82, 0x79, 0x2e, 0x33, 0xda,
+	0x0b, 0xaa, 0xa4, 0x82, 0xd5, 0xd2, 0x1f, 0xb8, 0xc9, 0x86, 0x36, 0x87, 0x91, 0xb1, 0x2f, 0x84,
+	0xcc, 0x31, 0xd7, 0xeb, 0x0c, 0x13, 0xe4, 0xe9, 0xf6, 0xdd, 0x6a, 0x7f, 0x93, 0x01, 0x0c, 0x42,
+	0x34, 0x04, 0xc7, 0xfa, 0x4d, 0x51, 0x54, 0xf3, 0xdf, 0x41, 0x2f, 0x44, 0x7d, 0x72, 0xaf, 0x13,
+	0xee, 0x09, 0xb4, 0x22, 0xa9, 0xd3, 0xb7, 0x92, 0x1e, 0x29, 0xfe, 0x01, 0x47, 0x67, 0xe0, 0x85,
+	0xa8, 0xe9, 0xd0, 0x1d, 0xba, 0x1e, 0x7d, 0x7a, 0x74, 0x6a, 0xb2, 0x9b, 0x81, 0xb7, 0x2e, 0x0e,
+	0x00, 0x57, 0xdd, 0x3e, 0x50, 0xd5, 0x43, 0xe7, 0xd0, 0xfa, 0xcd, 0x9e, 0x5e, 0x3a, 0xf5, 0xa0,
+	0x30, 0xff, 0xa2, 0x2e, 0x28, 0x51, 0xbe, 0xb6, 0xec, 0x4b, 0x7d, 0xfa, 0x09, 0x00, 0x00, 0xff,
+	0xff, 0xea, 0xd9, 0x75, 0xf2, 0xb8, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -188,9 +437,12 @@ type ChordClient interface {
 	GetPredecessor(ctx context.Context, in *NN, opts ...grpc.CallOption) (*Node, error)
 	// GetSuccessor returns current node's successor.
 	GetSuccessor(ctx context.Context, in *NN, opts ...grpc.CallOption) (*Node, error)
-	// Notify notifies Chord that Node thinks it is our predecessor. This has
-	// the potential to initiate the transferring of keys.
+	// Notify notifies Chord that Node might be the predecessor.
 	Notify(ctx context.Context, in *Node, opts ...grpc.CallOption) (*NN, error)
+	// For KVStore
+	Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetReply, error)
+	Put(ctx context.Context, in *PutRequest, opts ...grpc.CallOption) (*PutReply, error)
+	Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*DeleteReply, error)
 }
 
 type chordClient struct {
@@ -246,6 +498,33 @@ func (c *chordClient) Notify(ctx context.Context, in *Node, opts ...grpc.CallOpt
 	return out, nil
 }
 
+func (c *chordClient) Get(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetReply, error) {
+	out := new(GetReply)
+	err := c.cc.Invoke(ctx, "/chordrpc.Chord/Get", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *chordClient) Put(ctx context.Context, in *PutRequest, opts ...grpc.CallOption) (*PutReply, error) {
+	out := new(PutReply)
+	err := c.cc.Invoke(ctx, "/chordrpc.Chord/Put", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *chordClient) Delete(ctx context.Context, in *DeleteRequest, opts ...grpc.CallOption) (*DeleteReply, error) {
+	out := new(DeleteReply)
+	err := c.cc.Invoke(ctx, "/chordrpc.Chord/Delete", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ChordServer is the server API for Chord service.
 type ChordServer interface {
 	// FindSuccessor returns successor node of ID. Could initiate other RPC calls.
@@ -256,9 +535,12 @@ type ChordServer interface {
 	GetPredecessor(context.Context, *NN) (*Node, error)
 	// GetSuccessor returns current node's successor.
 	GetSuccessor(context.Context, *NN) (*Node, error)
-	// Notify notifies Chord that Node thinks it is our predecessor. This has
-	// the potential to initiate the transferring of keys.
+	// Notify notifies Chord that Node might be the predecessor.
 	Notify(context.Context, *Node) (*NN, error)
+	// For KVStore
+	Get(context.Context, *GetRequest) (*GetReply, error)
+	Put(context.Context, *PutRequest) (*PutReply, error)
+	Delete(context.Context, *DeleteRequest) (*DeleteReply, error)
 }
 
 // UnimplementedChordServer can be embedded to have forward compatible implementations.
@@ -279,6 +561,15 @@ func (*UnimplementedChordServer) GetSuccessor(ctx context.Context, req *NN) (*No
 }
 func (*UnimplementedChordServer) Notify(ctx context.Context, req *Node) (*NN, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Notify not implemented")
+}
+func (*UnimplementedChordServer) Get(ctx context.Context, req *GetRequest) (*GetReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Get not implemented")
+}
+func (*UnimplementedChordServer) Put(ctx context.Context, req *PutRequest) (*PutReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Put not implemented")
+}
+func (*UnimplementedChordServer) Delete(ctx context.Context, req *DeleteRequest) (*DeleteReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
 }
 
 func RegisterChordServer(s *grpc.Server, srv ChordServer) {
@@ -375,6 +666,60 @@ func _Chord_Notify_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Chord_Get_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChordServer).Get(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chordrpc.Chord/Get",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChordServer).Get(ctx, req.(*GetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Chord_Put_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PutRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChordServer).Put(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chordrpc.Chord/Put",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChordServer).Put(ctx, req.(*PutRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Chord_Delete_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ChordServer).Delete(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/chordrpc.Chord/Delete",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ChordServer).Delete(ctx, req.(*DeleteRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Chord_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "chordrpc.Chord",
 	HandlerType: (*ChordServer)(nil),
@@ -398,6 +743,18 @@ var _Chord_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Notify",
 			Handler:    _Chord_Notify_Handler,
+		},
+		{
+			MethodName: "Get",
+			Handler:    _Chord_Get_Handler,
+		},
+		{
+			MethodName: "Put",
+			Handler:    _Chord_Put_Handler,
+		},
+		{
+			MethodName: "Delete",
+			Handler:    _Chord_Delete_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
